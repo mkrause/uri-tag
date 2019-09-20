2291bb161517510e6268d458cd087713719c0982
mkrause <maikelkrause@gmail.com>
2019-09-20 20:09:56 +0200

    Add TypeScript declaration.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 
 # Change Log
 
+- v1.0.0
+  - First stable release.
+  - Adds support for TypeScript.
+
 - v0
-  - Initial version
+  - Initial version.
---
+++ typings/uri-tag.d.ts
@@ -0,0 +1,13 @@
+
+declare module 'uri-tag' {
+    export const rawSymbol : symbol;
+    
+    export const raw : <T>(value : T) => { [key in typeof rawSymbol] : T };
+    
+    const uri : {
+        (stringParts : TemplateStringsArray, ...substitutions : unknown[]) : string,
+        raw : typeof raw,
+    };
+    
+    export default uriExport;
+}
